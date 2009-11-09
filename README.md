4390764cf15967272609c49efe74d05ee14d0c04
leevigraham <leevi@newism.com.au>
2009-11-09 12:02:24 +1100

    Added some inline docs to the extension file

---
+++ README.md
@@ -0,0 +1,37 @@
+NSM Addon Updater 1.0.0a1
+=========================
+
+NSM Addon Updater is an EE 2.0 extension that checks an external RSS feed for version updates and displays them on your CP homepage.
+
+If you want to include NSM Addon updater support in your addon just add the following public variable to any extension class:
+
+	public $versions_xml = "http://yourdomain.com/versions.xml";
+
+The url should point to a valid RSS 2.0 XML feed that lists individual versions of your addon as <items>. There is only one required addition to a standard feed: <ee_addon:version>1.0.0b1</ee_addon:version> which is used for version comparison.
+
+Each feed is individually cached so that the CURL calls don't stall the loading of the CP. Additionally the calls are made via AJAX so there should be no negative affect on CP load.
+
+Example RSS 2.0 XML Feed:
+
+	<?xml version="1.0" encoding="utf-8"?>
+	<rss version="2.0" xmlns:ee_addon="http://expressionengine-addons.com/nsm_addon_updater/#rss-xml">
+		<channel>
+			<title>NSM Addon Updater Changelog</title>
+			<link>http://yourdomain.com/nsm.addon_updater.ee_addon/appcast.xml</link>
+			<description>Most recent changes with links to updates.</description>
+			<item>
+				<title>Version 1.0.0b1</title>
+				<ee_addon:version>1.0.0b1</ee_addon:version>
+				<link>http://yourdomain.com/nsm.addon_updater.ee_addon/1.0.0b1/</link>
+				<pubDate>Wed, 09 Jan 2006 19:20:11 +0000</pubDate>
+				<description><![CDATA[
+					<ul>
+						<li>Added the {selected_group_id} variable for available use in the User Key Notification Template.</li>
+						<li>Added the form:attribute="" parameter type to all User functions that output forms.</li>
+					</ul>
+				]]>
+				</description>
+				<enclosure url="http://expressionengine-addons.com/nsm.addon_updater.ee_addon/download.zip?version=1.0.0b1" length="1623481" type="application/zip" />
+			 </item>
+		</channel>
+	</rss>
--- expressionengine/system/third_party/nsm_addon_updater/ext.nsm_addon_updater.php
+++ expressionengine/system/third_party/nsm_addon_updater/ext.nsm_addon_updater.php
@@ -5,6 +5,43 @@
  * @package NSM
  * @subpackage Addon Updater
  * @author Leevi Graham
+ * 
+ * NSM Addon Updater is an EE 2.0 extension that checks an external RSS feed for version updates and displays them on your homepage.
+ * 
+ * If you want to include NSM Addon updater support in your addon just add the following public variable to any extension class:
+ * 
+ * 		public $versions_xml = "http://yourdomain.com/versions.xml";
+ * 
+ * The url should point to a valid RSS 2.0 XML feed that lists individual versions of your addon as <items>. 
+ * There is only one required addition to a standard feed: <ee_addon:version>1.0.0b1</ee_addon:version> which is used for version comparison.
+ * 
+ * Each feed is individually cached so that the CURL calls don't stall the loading of the CP. 
+ * Additionally the calls are made via AJAX so there should be no negative affect on CP load.
+ * 
+ * Example RSS 2.0 XML Feed:
+ * 
+ * <?xml version="1.0" encoding="utf-8"?>
+ * <rss version="2.0" xmlns:ee_addon="http://yourdomain.com/nsm_addon_updater/#rss-xml">
+ * 	<channel>
+ * 		<title>NSM Addon Updater Changelog</title>
+ * 		<link>http://yourdomain.com/nsm.addon_updater.ee_addon/appcast.xml</link>
+ * 		<description>Most recent changes with links to updates.</description>
+ * 		<item>
+ * 			<title>Version 1.0.0b1</title>
+ * 			<ee_addon:version>1.0.0b1</ee_addon:version>
+ * 			<link>http://yourdomain.com/nsm.addon_updater.ee_addon/1.0.0b1/</link>
+ * 			<pubDate>Wed, 09 Jan 2006 19:20:11 +0000</pubDate>
+ * 			<description><![CDATA[
+ * 				<ul>
+ * 					<li>Added the {selected_group_id} variable for available use in the User Key Notification Template.</li>
+ * 					<li>Added the form:attribute="" parameter type to all User functions that output forms.</li>
+ * 				</ul>
+ * 			]]>
+ * 			</description>
+ * 			<enclosure url="http://yourdomain.com/nsm.addon_updater.ee_addon/download.zip?version=1.0.0b1" length="1623481" type="application/zip" />
+ * 		 </item>
+ * 	</channel>
+ * </rss>
  **/
 class Nsm_addon_updater_ext
 {
@@ -12,7 +49,7 @@ class Nsm_addon_updater_ext
 	public $name = "NSM Addon Updater";
 	public $version = '1.0.0a1';
 	public $docs_url = "http://leevigraham.com/";
-	public $versions_xml = "http://dev.expressionengine-addons.com/versions.xml";
+	public $versions_xml = "http://yourdomain.com/versions.xml";
 
 	public $settings_exist = "y";
 	private $default_settings = array(
