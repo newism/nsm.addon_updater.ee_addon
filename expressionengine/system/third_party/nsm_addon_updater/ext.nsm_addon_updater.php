294ffeae07f69287376d810b219a4cb353c8316e
Leevi Graham <leevi@newism.com.au>
2009-12-12 15:22:13 +1100

    Removed my site from docs string becuase no docs exist there

--- expressionengine/system/third_party/nsm_addon_updater/ext.nsm_addon_updater.php
+++ expressionengine/system/third_party/nsm_addon_updater/ext.nsm_addon_updater.php
@@ -48,7 +48,7 @@ class Nsm_addon_updater_ext
 	public $addon_name = "NSM Addon Updater";
 	public $name = "NSM Addon Updater";
 	public $version = '1.0.0a2';
-	public $docs_url = "http://leevigraham.com/";
+	public $docs_url = "";
 	public $versions_xml = "https://github.com/newism/nsm.addon_updater.ee_addon/raw/master/expressionengine/system/third_party/nsm_addon_updater/versions.xml";
 
 	public $settings_exist = "y";
