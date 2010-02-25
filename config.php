2f4266e68b9b25399f937183d85bd9d70245e60c
Leevi Graham <leevi@newism.com.au>
2010-02-26 00:10:37 +1100

    Added required config vars back in

--- config.php
+++ config.php
@@ -1,5 +1,8 @@
 <?php
 
+$config['name']				= 'NSM Addon Updater';
+$config['version']	 		= '0.1.0';
+
 $config['nsm_addon_updater']['versions_xml'] 			= 'http://github.com/newism/nsm.addon_updater.ee_addon/raw/master/versions.xml';
-//$config['nsm_addon_updater']['versions_xml'] 			= 'http://local.expressionengine-addons.com/system/expressionengine/third_party/nsm_addon_updater/test_versions.xml';
+$config['nsm_addon_updater']['versions_xml'] 			= 'http://local.expressionengine-addons.com/system/expressionengine/third_party/nsm_addon_updater/test_versions.xml';
 $config['nsm_addon_updater']['custom_download_url'] 	= array('Nsm_addon_updater_acc', 'nsm_addon_updater_download_url');
