20782d834074249f4bc0815d8aa7d1db037fbc20
Leevi Graham <leevi@newism.com.au>
2010-02-25 23:58:51 +1100

    Changed the version number back to 0.1.0

--- acc.nsm_addon_updater.php
+++ acc.nsm_addon_updater.php
@@ -4,11 +4,11 @@ class Nsm_addon_updater_acc
 {
 	var $name	 		= 'NSM Add-on Updater';
 	var $id	 			= 'nsm_addon_updater';
-	var $version	 	= '1.0';
+	var $version	 	= '0.1.0';
 	var $description	= 'Accessory for NSM Addon Updater.';
 	var $sections	 	= array();
 	var $cache_lifetime	= 86400;
-	
+
 	/**
 	* Set Sections
 	*
