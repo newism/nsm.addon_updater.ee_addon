8000a72c1dcc5fd8b6dc00840e291e5627bfbb84
Leevi Graham <leevi@newism.com.au>
2010-07-07 16:13:56 +1000

    Code comment updates

--- acc.nsm_addon_updater.php
+++ acc.nsm_addon_updater.php
@@ -5,8 +5,8 @@
  *
  * @package			NsmAddonUpdater
  * @version			1.0.0
- * @author			Leevi Graham <http://leevigraham.com>
- * @copyright 		Copyright (c) 2007-2010 Newism
+ * @author			Leevi Graham <http://leevigraham.com> - Technical Director, Newism
+ * @copyright 		Copyright (c) 2007-2010 Newism <http://newism.com.au>
  * @license 		Commercial - please see LICENSE file included with this distribution
  * @link			http://expressionengine-addons.com/nsm-example-addon
  * @see				http://expressionengine.com/public_beta/docs/development/accessories.html
--- config.php
+++ config.php
@@ -5,11 +5,10 @@
  *
  * @package			NsmAddonUpdater
  * @version			1.0.0
- * @author			Leevi Graham <http://leevigraham.com>
- * @copyright 		Copyright (c) 2007-2010 Newism
+ * @author			Leevi Graham <http://leevigraham.com> - Technical Director, Newism
+ * @copyright 		Copyright (c) 2007-2010 Newism <http://newism.com.au>
  * @license 		Commercial - please see LICENSE file included with this distribution
  * @link			http://expressionengine-addons.com/nsm-addon-updater
- * @see			http://expressionengine-addons.com/nsm-example-addon
  */
 
 
