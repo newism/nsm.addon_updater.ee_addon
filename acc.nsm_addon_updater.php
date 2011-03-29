b811c682ccc44dbe498eca0680d88480dd19ea85
Leevi Graham <leevi@newism.com.au>
2011-03-29 23:12:47 +1100

    Updated url for ee-garage.com

--- acc.nsm_addon_updater.php
+++ acc.nsm_addon_updater.php
@@ -8,7 +8,7 @@
  * @author			Leevi Graham <http://leevigraham.com> - Technical Director, Newism
  * @copyright 		Copyright (c) 2007-2010 Newism <http://newism.com.au>
  * @license 		Commercial - please see LICENSE file included with this distribution
- * @link			http://expressionengine-addons.com/nsm-example-addon
+ * @link			http://ee-garage.com/nsm-example-addon
  * @see				http://expressionengine.com/public_beta/docs/development/accessories.html
  */
 
--- config.php
+++ config.php
@@ -8,14 +8,14 @@
  * @author			Leevi Graham <http://leevigraham.com> - Technical Director, Newism
  * @copyright 		Copyright (c) 2007-2010 Newism <http://newism.com.au>
  * @license 		Commercial - please see LICENSE file included with this distribution
- * @link			http://expressionengine-addons.com/nsm-addon-updater
+ * @link			http://ee-garage.com/nsm-addon-updater
  */
 
 
 $config['name']				= 'NSM Addon Updater';
 $config['version']	 		= '1.0.1';
 
-$config['nsm_addon_updater']['versions_xml'] 			= 'http://expressionengine-addons.com/nsm-addon-updater/release-notes/feed';
+$config['nsm_addon_updater']['versions_xml'] 			= 'http://ee-garage.com/nsm-addon-updater/release-notes/feed';
 $config['nsm_addon_updater']['custom_download_url'] 	= array('Nsm_addon_updater_acc', 'nsm_addon_updater_download_url');
 
 // Local test XML
