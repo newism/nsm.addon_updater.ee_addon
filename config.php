10879f6c7aa3c1ef06ee4549449921c0346689e9
Leevi Graham <leevi@newism.com.au>
2011-03-29 23:16:29 +1100

    Version number / url changes

--- config.php
+++ config.php
@@ -4,7 +4,7 @@
  * Config file for NSM Addon Updater
  *
  * @package			NsmAddonUpdater
- * @version			1.0.1
+ * @version			1.1.0
  * @author			Leevi Graham <http://leevigraham.com> - Technical Director, Newism
  * @copyright 		Copyright (c) 2007-2010 Newism <http://newism.com.au>
  * @license 		Commercial - please see LICENSE file included with this distribution
@@ -13,7 +13,7 @@
 
 
 $config['name']				= 'NSM Addon Updater';
-$config['version']	 		= '1.0.1';
+$config['version']	 		= '1.1.0';
 
 $config['nsm_addon_updater']['versions_xml'] 			= 'http://ee-garage.com/nsm-addon-updater/release-notes/feed';
 $config['nsm_addon_updater']['custom_download_url'] 	= array('Nsm_addon_updater_acc', 'nsm_addon_updater_download_url');
