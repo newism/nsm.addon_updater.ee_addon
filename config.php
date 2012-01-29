5bfd847e6fa839cecd945b65ac729865875948aa
Iain Saxon <iain.saxon@newism.com.au>
2012-01-30 10:26:32 +1100

    version bump to 1.1.1

--- config.php
+++ config.php
@@ -4,16 +4,16 @@
  * Config file for NSM Addon Updater
  *
  * @package			NsmAddonUpdater
- * @version			1.1.0
+ * @version			1.1.1
  * @author			Leevi Graham <http://leevigraham.com> - Technical Director, Newism
- * @copyright 		Copyright (c) 2007-2010 Newism <http://newism.com.au>
+ * @copyright 		Copyright (c) 2007-2012 Newism <http://newism.com.au>
  * @license 		Commercial - please see LICENSE file included with this distribution
  * @link			http://ee-garage.com/nsm-addon-updater
  */
 
 
 $config['name']				= 'NSM Addon Updater';
-$config['version']	 		= '1.1.0';
+$config['version']	 		= '1.1.1';
 
 $config['nsm_addon_updater']['versions_xml'] 			= 'http://ee-garage.com/nsm-addon-updater/release-notes/feed';
 $config['nsm_addon_updater']['custom_download_url'] 	= array('Nsm_addon_updater_acc', 'nsm_addon_updater_download_url');
