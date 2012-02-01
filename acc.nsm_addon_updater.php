da81bec7f1d887cd30b746954cef64d1bfdbcd28
Iain Saxon <iain.saxon@newism.com.au>
2012-02-02 10:57:43 +1100

    added exit to ajax process method

--- acc.nsm_addon_updater.php
+++ acc.nsm_addon_updater.php
@@ -142,7 +142,8 @@ class Nsm_addon_updater_acc
 		$EE->cp->load_package_js("accessory_tab");
 		$EE->cp->load_package_css("accessory_tab");
 
-		echo $EE->load->view("/accessory/updates", array('versions' => $versions), TRUE); 
+		echo $EE->load->view("/accessory/updates", array('versions' => $versions), TRUE);
+		exit;
 	}
 
 	// =======================
