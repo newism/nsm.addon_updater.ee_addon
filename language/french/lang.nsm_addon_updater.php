b9ef5a862c6a59731339aff1050c1d259fc94b55
Emmanuel Beauchamps <e.bops@hotmail.fr>
2012-10-21 17:58:47 +0200

    French language pack
    Creating the French translation for NSM addon updater

---
+++ language/french/lang.nsm_addon_updater.php
@@ -0,0 +1,52 @@
+<?php
+
+/**
+ * NSM Addon Updater Language File
+ *
+ * @package			NsmAddonUpdater
+ * @version			1.2.1
+ * @author			Leevi Graham <http://leevigraham.com> - Technical Director, Newism
+ * @copyright 		Copyright (c) 2007-2012 Newism <http://newism.com.au>
+ * @license 		Commercial - please see LICENSE file included with this distribution
+ * @link			http://ee-garage.com/nsm-addon-updater
+ * @see				http://expressionengine.com/public_beta/docs/development/modules.html#lang_file
+ */
+$lang = array(
+
+	'global_settings_title'
+	=> 'Paramètres généraux',
+
+	'global_settings_info'
+	=> 'Les paramètres généraux sont appliqués à tous vos sites.',
+
+	'enable_extension_title'
+	=> 'Rechercher les MAJ des extensions créées par des développeurs tiers ?',
+
+	'enable_extension_info'
+	=> 'Pour les extensions qui l\'autorise, NSM Addon Updater recherche les éventuelles MAJ de vos Plugins, Extensions et Modules. Les résultats sont stockés en cache pour une période définie.',
+
+	'enable_extension_label'
+	=> 'Souhaitez-vous que NSM Addon Updater recherche des MAJ et qu\'il les fasse apparaître sur la page d\'accueil de votre espace d\'administration ?',
+
+	'cache_expiration_label'
+	=> 'Pendant combien de jours souhaitez-vous que les recherches de MAJ soient stockées en cache ?',
+
+	'extension_access_title'
+	=> 'Autorisation d\'accès à l\'extension',
+
+	'member_group_access_label'
+	=> 'Quels groupes de membre verront le message de MAJ ?',
+
+	'extension_settings_saved_success'
+	=> 'Les paramètres de l\'extension ont été enregistrés avec succès',
+
+	'nsm_addon_updater_disabled_notice'
+	=> '<a href="http://ee-garage.com/nsm-addon-updater/">NSM Addon Updater</a> doit être installé et activé pour que vous ayez accès à cette fonction.',
+
+	'save_extension_settings'
+	=> 'Enregistrer les paramètres d\'extension',
+
+	'cache_expiration_field'
+	=> 'Expiration du cache',
+
+);
